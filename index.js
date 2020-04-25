3242be71f90c8f62102947a93596ffe7a0f682bd
Alfred-Lau <jian.liu@icloud.com>
2020-04-25 19:55:48 +0800

    feat: add xss filter

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const vm = require('vm');
 
 const user = {
-  name: 'liujian',
+  name: '<script />',
 };
 
-const res = vm.runInNewContext('`<h2>hello, ${user.name}</h2>`', { user });
+const res = vm.runInNewContext('`<h2>hello, ${_(user.name)}</h2>`', {
+  user,
+  /* 简易的 XSS 过滤函数 */
+  _: function (markup) {
+    if (!markup) {
+      return '';
+    }
+    return String(markup)
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&#39;')
+      .replace(/"/g, '&quot;');
+  },
+});
 
 console.log(res);
--- note.md
+++ note.md
@@ -1,2 +1,19 @@
 # fast-template-engine
+
 基于 es6 模板字符串实现的模板引擎，追求渲染的高效
+
+1. 简易的 XSS 过滤函数
+
+```js
+_: function (markup) {
+    if (!markup) {
+      return '';
+    }
+    return String(markup)
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/'/g, '&#39;')
+      .replace(/"/g, '&quot;');
+  },
+```
