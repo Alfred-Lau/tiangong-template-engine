49b130f6e6886e7b79a91af0629ea79ef58f4164
Alfred-Lau <jian.liu@icloud.com>
2020-06-11 01:40:39 +0800

    feat: add basic

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const vm = require('vm');
 
+/*
+
+// basic
+const vm = require('vm');
+
+const user = {
+  name: 'liujian',
+};
+
+const res = vm.runInNewContext('`<h2>hello, ${user.name}</h2>`', { user });
+
+console.log(res);
+
+
+*/
+
 const user = {
   name: '<script />',
 };
